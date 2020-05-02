b30a2fd75ee42a90704698f7e95b8bb9685470b4
Luis Lloret <luislloret@gmail.com>
2020-05-02 15:39:04 +0100

    Scheduler callback initial implementation

--- src/sp_midi.erl
+++ src/sp_midi.erl
@@ -1,6 +1,6 @@
 -module(sp_midi).
 -export([midi_init/0, midi_deinit/0, midi_send/1, midi_ins/0, midi_outs/0, have_my_pid/0, 
-        set_this_pid/1, set_log_level/1, get_current_time_microseconds/0]).
+        set_this_pid/1, set_log_level/1, schedule_callback/3, get_current_time_microseconds/0]).
 -on_load(init/0).
 
 init() ->
@@ -15,7 +15,7 @@ midi_init() ->
     exit(nif_library_not_loaded).
 midi_deinit() ->
     exit(nif_library_not_loaded).
-midi_send(_X) ->
+midi_send(_) ->
     exit(nif_library_not_loaded).
 midi_ins() ->
     exit(nif_library_not_loaded).
@@ -25,7 +25,9 @@ have_my_pid() ->
     exit(nif_library_not_loaded).
 get_current_time_microseconds() ->
     exit(nif_library_not_loaded).
-set_log_level(_X) ->
+set_log_level(_) ->
     exit(nif_library_not_loaded).
-set_this_pid(_X) ->
+set_this_pid(_) ->
+    exit(nif_library_not_loaded).
+schedule_callback(_, _, _) ->
     exit(nif_library_not_loaded).
--- src/sp_midi_test.erl
+++ src/sp_midi_test.erl
@@ -1,5 +1,5 @@
 -module(sp_midi_test).
--export([start/0, midi_process/0, test_get_current_time_microseconds/2]).
+-export([start/0, midi_process/0, test_get_current_time_microseconds/2, test_scheduler_callback_process/0]).
 
 
 midi_process() ->
@@ -13,6 +13,17 @@ midi_process() ->
 
     end,
     midi_process().
+
+test_scheduler_callback_process() ->
+    receive
+        X when is_integer(X) ->
+            Treceived = sp_midi:get_current_time_microseconds(),
+            io:fwrite("Received callback message   : ~p~n", [X]),
+            io:fwrite("Received at                 : ~p~n", [Treceived]);
+        _ ->
+            io:fwrite("Received something else~n")
+    end,
+    test_scheduler_callback_process().
 
 
 test_get_current_time_microseconds(0, _) ->
@@ -28,7 +39,7 @@ start() ->
 %    cd("d:/projects/sp_midi/src").
     compile:file(sp_midi),
 
-    io:fwrite("Testing NIF function to return current time in microseconds. The values should be around 1000 miliseconds away"),
+    io:fwrite("Testing NIF function to return current time in microseconds. The values should be around 1000 miliseconds away~n"),
     test_get_current_time_microseconds(3, 1000),
 
     Aon = binary:list_to_bin("/*/note_on"),
@@ -38,15 +49,26 @@ start() ->
 
     sp_midi:midi_init(),
 
+    PidTestSchedulerCallback = spawn(sp_midi_test, test_scheduler_callback_process, []),
+    io:fwrite("PidTestSchedulerCallback ~p~n", [PidTestSchedulerCallback]),
+    T = sp_midi:get_current_time_microseconds(),
+    Tcallback = T + 3000000,
+    io:fwrite("Time now is                 : ~p~n", [T]),
+    io:fwrite("Want to fire the callback at: ~p~n", [Tcallback]),
+    sp_midi:schedule_callback(Tcallback, PidTestSchedulerCallback, 42),
+    sp_midi:schedule_callback(Tcallback + 10000, PidTestSchedulerCallback, 43), 
+    sp_midi:schedule_callback(Tcallback + 20000, PidTestSchedulerCallback, 44), 
+    sp_midi:schedule_callback(Tcallback - 10000, PidTestSchedulerCallback, 45), 
+
     Pid = spawn(sp_midi_test, midi_process, []),
     sp_midi:set_this_pid(Pid),
 
     INS = sp_midi:midi_ins(),
     OUTS = sp_midi:midi_outs(),
 
-    io:fwrite("MIDI INs:~p~n", [INS]),
+    %io:fwrite("MIDI INs:~p~n", [INS]),
 
-    io:fwrite("MIDI OUTs:~p~n", [OUTS]),
+    %io:fwrite("MIDI OUTs:~p~n", [OUTS]),
     
     io:fwrite("Sending note ON and waiting 3 seconds~n"),
     sp_midi:midi_send(Mon),
